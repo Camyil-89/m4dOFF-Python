6481e9fd7d5e97c6675b71933c361e2f706b4846
Camyil-89 <zhukovkd@yandex.ru>
2023-12-23 21:55:35 +0300

    .

--- executing.py
+++ executing.py
@@ -25,15 +25,17 @@ from xml.dom import minidom
 import datetime
 from parser_docx import Parser
 import re
+import os
+import logging
 
 
 class Template():
     def __init__(self, path):
+        self.name_template = "Пример работы шаблона"
         '''
         Создает основную часть документа
         '''
         self.path_to_source = path
-        self.name_template = "None"
 
         self.parser = Parser()  # парсер .docx файлов
         self.parser.set_path(path)  # выбираем файл
@@ -55,23 +57,44 @@ class Template():
         xml_text = reparsed.toprettyxml(indent="    ")
         with open(f'{path_folder}{os.sep}{self.name}_{self.uid}.xml', "w", encoding="utf-8") as file:
             file.write(xml_text)
-        #with open(f'test.xml', "w", encoding="utf-8") as file:
-
         return f'{path_folder}{os.sep}{self.name}_{self.uid}.xml'
 
     def __from_string(self, text: str):
-        xml_string = re.sub('>[\s\\n\\r]+<', '><', text)
-        return ET.fromstring(xml_string)
+        try:
+            xml_string = re.sub('>[\s\\n\\r]+<', '><', text)
+            return ET.fromstring(xml_string)
+        except Exception as ex:
+            print(f"Ошибка в XML:\\n{text}")
+            logging.exception(ex)
+            raise ex
+
+    def __add_attribute(self, path, property, value):
+        element = self.__find_element(path)
+        element.attrib.update({f"{property}": f"{value}"})
+
     def __find_element(self, path):
         return self.root.find(path)
 
+    def __get_patronymic(self, fio_parts):
+        if isinstance(fio_parts, list) == False:
+            fio_parts = f"{fio_parts}".split(" ")
+        patronymic = f' Отчество="{fio_parts[-1].upper()}"' if len(fio_parts) == 3 else ''
+        return patronymic
+
+    def __get_fio(self, fio):
+        fio_parts = fio.split(" ")
+        return f'Фамилия="{fio_parts[0].upper()}" Имя="{fio_parts[1].upper()}" {self.__get_patronymic(fio_parts)}'
+
+    def __get_passport_number(self, number):
+        numbers_only = number.replace(" ", "")
+        formatted = f"{numbers_only[:2]} {numbers_only[2:4]} {numbers_only[4:]}"
+        return formatted
 
     def custom(self):
         '''
         данная часть модифицируется
         :return:
         '''
-        pass
 """)
     def __read_file(self, path):
         with open(path, "r", encoding="utf-8") as file:  # Используем бинарный режим записи
